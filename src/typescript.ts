6f5a5186b9338e3412cb86b28c8f1f6329921ef7
Filip Kotev <filipkotev@yahoo.com>
2024-12-01 16:14:58 +0200

    Arrays and Objects

--- src/typescript.ts
+++ src/typescript.ts
@@ -26,3 +26,31 @@ orderStatus = 'delivered';
 let discount: number | string = 20;
 discount = '20%';
 // console.log(discount);
+
+/**
+ * Arrays and Objects
+ */
+let temperatures: number[] = [20, 35, 30];
+// temperatures.push('hot'); // This will result in a TypeScript error
+
+let colors: string[] = ['red', 'blue', 'orange'];
+// colors.push(true); // This will result in Typescript error
+
+let mixedArray: (string | number)[] = [1, 'two', 3];
+// mixedArray.push(true); // This will result in a TypeScript error
+
+
+// 1. Bike
+let bike: { brand: string; year: number } = { brand: 'Yamaha', year: 2010 };
+// bike.year = 'old'; // This will result in a TypeScript error
+
+// 2. Laptop
+let laptop: { brand: string, year: number } = { brand: 'Dell', year: 2023 };
+// let laptop2: { brand: string, year: number } = {brand: 'HP'} // This will result in Typescript error that year is missing
+
+// 3. Products
+let product1 = { title: 'Shirt', price: 20};
+let product2 = { title: 'pants', price: 'free'};
+let product3 = { title: 'Shoes'};
+let products: { title: string, price?: number | string }[] = [product1, product2, product3];
+// products.push({ title: 'Shoes', price: 'expensive' }); // This will result in a TypeScript error
