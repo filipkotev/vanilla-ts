7388c24fe4140ea351bc9d0093a2b46f2aff5358
Filip Kotev <filipkotev@yahoo.com>
2024-12-01 15:42:21 +0200

    Union and Any type

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,13 +1,28 @@
+/**
+ * Type Annotation and Type Inference
+ */
 let name: string = 'John Doe';
 
 const lowerCaseName = name.toLowerCase();
-console.log(lowerCaseName);
+// console.log(lowerCaseName);
 
 let age:number = 17;
 const newAge = age + 1;
-console.log(newAge);
+// console.log(newAge);
 
 let isAdult = newAge >= 18;
 isAdult = !isAdult;
 
-console.log(isAdult);
+// console.log(isAdult);
+
+/**
+ * Union and Any Type
+ */
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+// console.log('Order status: ', orderStatus);
+
+let discount: number | string = 20;
+discount = '20%';
+// console.log(discount);
