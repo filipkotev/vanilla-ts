52e484820cd1181cc75f8528b312c9643e2b9469
Filip Kotev <filipkotev@yahoo.com>
2024-12-01 15:26:31 +0200

    Type Annotations and Type Inference

---
+++ src/typescript.ts
@@ -0,0 +1,13 @@
+let name: string = 'John Doe';
+
+const lowerCaseName = name.toLowerCase();
+console.log(lowerCaseName);
+
+let age:number = 17;
+const newAge = age + 1;
+console.log(newAge);
+
+let isAdult = newAge >= 18;
+isAdult = !isAdult;
+
+console.log(isAdult);
